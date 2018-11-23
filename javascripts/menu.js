92d4380042755e9a38cf60124b1101092a3b0173
ronnen <ronnen.armon@capriza.com>
2018-11-23 16:38:03 +0200

    fake custom filter and new fontawesome

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -2,6 +2,7 @@
 function drawMenu(criteria) {
   // criteria {totalValueMin, totalValueMax, waitTimeMin, waitTimeMax}
 
+  var timeRangeMin = criteria.timeRangeMin || 0, timeRangeMax = criteria.timeRangeMax || 100;
   var totalValueMin = criteria.totalValueMin || 0, totalValueMax = criteria.totalValueMax || 100000000;
   var waitTimeMin = criteria.waitTimeMin || 0, waitTimeMax = criteria.waitTimeMax || 100000000;
 
@@ -10,16 +11,43 @@ function drawMenu(criteria) {
   d3.select("#wait-time-filter .label-left").text(waitToText(waitTimeMin));
   d3.select("#wait-time-filter .label-right").text(waitToText(waitTimeMax));
 
-  function switchClick() {
+  function drawOverviewByCriteria() {
+    var currentTimeRange = valueSlider.range();
     var currentValueRange = valueSlider.range();
     var currentWaitRange = waitSlider.range();
     drawOverview(filterDataByCriteria(mainUnits, {
+      timeRangeMin: timeFilterState ? currentTimeRange.begin : null,
+      timeRangeMax: timeFilterState ? currentTimeRange.end : null,
       totalValueMin: valueFilterState ? currentValueRange.begin : null,
       totalValueMax: valueFilterState ? currentValueRange.end : null,
       waitTimeMin: waitFilterState ? currentWaitRange.begin : null,
       waitTimeMax: waitFilterState ? currentWaitRange.end : null,
     }));
   }
+
+  // Time Range filter
+  var timeFilterState = true;
+
+  d3.select("#time-range-filter .switch-container")
+    .on("click", timeSliderClick);
+
+  function timeSliderClick() {
+    d3.select("#time-range-filter").classed("on", !timeFilterState);
+    timeFilterState = !timeFilterState;
+    drawOverviewByCriteria();
+  }
+
+  var timeSlider = createD3RangeSlider(timeRangeMin, timeRangeMax, "#time-range-filter .stats-slider", false);
+
+  timeSlider.onChange(function(newRange){
+    // d3.select("#range-label").html(newRange.begin + " &mdash; " + newRange.end);
+  });
+
+  timeSlider.onRelease(function(newRange){
+    drawOverviewByCriteria();
+  });
+
+  timeSlider.range(timeRangeMin,timeRangeMax);
 
   // Total Value filter
   var valueFilterState = true;
@@ -30,7 +58,7 @@ function drawMenu(criteria) {
   function valueSliderClick() {
     d3.select("#total-value-filter").classed("on", !valueFilterState);
     valueFilterState = !valueFilterState;
-    switchClick();
+    drawOverviewByCriteria();
   }
 
   var valueSlider = createD3RangeSlider(totalValueMin, totalValueMax, "#total-value-filter .stats-slider", false);
@@ -40,17 +68,7 @@ function drawMenu(criteria) {
   });
 
   valueSlider.onRelease(function(newRange){
-    // d3.select("#range-label").html(newRange.begin + " &mdash; " + newRange.end);
-    // console.log("value range changed " + newRange.begin + " " + newRange.end);
-
-    totalValueMin = newRange.begin;
-    totalValueMax = newRange.end;
-    drawOverview(filterDataByCriteria(mainUnits, {
-      totalValueMin: totalValueMin,
-      totalValueMax: totalValueMax,
-      waitTimeMin: waitFilterState ? waitTimeMin : null,
-      waitTimeMax: waitFilterState ? waitTimeMax : null,
-    }));
+    drawOverviewByCriteria();
   });
 
   valueSlider.range(totalValueMin,totalValueMax);
@@ -64,25 +82,13 @@ function drawMenu(criteria) {
   function waitSliderClick() {
     d3.select("#wait-time-filter").classed("on", !waitFilterState);
     waitFilterState = !waitFilterState;
-    switchClick();
+    drawOverviewByCriteria();
   }
 
   var waitSlider = createD3RangeSlider(waitTimeMin, waitTimeMax, "#wait-time-filter .stats-slider", false);
 
-  /*
-   slider.onChange(function(newRange){
-   d3.select("#range-label").html(newRange.begin + " &mdash; " + newRange.end);
-   });
-   */
   waitSlider.onRelease(function(newRange){
-    waitTimeMin = newRange.begin;
-    waitTimeMax = newRange.end;
-    drawOverview(filterDataByCriteria(mainUnits, {
-      totalValueMin: valueFilterState ? totalValueMin : null,
-      totalValueMax: valueFilterState ? totalValueMax : null,
-      waitTimeMin: waitTimeMin,
-      waitTimeMax: waitTimeMax,
-    }));
+    drawOverviewByCriteria();
   });
 
   waitSlider.range(waitTimeMin,waitTimeMax);
